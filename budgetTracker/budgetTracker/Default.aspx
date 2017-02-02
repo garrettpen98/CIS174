142a048b1092233c5efceff95e61fea75bb6e5dc
garrettpen <garrettpen@gmail.com>
2017-02-01 19:54:07 -0600

    Initial Creation

---
+++ budgetTracker/budgetTracker/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="budgetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="salary" runat="server" Text="Please input monthly salary "></asp:Label>
+        <asp:TextBox ID="salaryInput" runat="server"></asp:TextBox>
+        <asp:Button ID="saveSalaryButton" runat="server" OnClick="saveSalaryButton_Click" Text="Save salary" />
+        <asp:Label ID="monthlySalaryLabel" runat="server" Text="$0.00"></asp:Label>
+    
+    </div>
+    <div id="expenseNameLabel">
+
+        Please input name of monthly expense&nbsp;
+        <asp:TextBox ID="expenseNameInput" runat="server"></asp:TextBox>
+        <asp:Label ID="expenseAmountLabel" runat="server" Text="Please enter amount of expense"></asp:Label>
+        <asp:TextBox ID="expenseAmountInput" runat="server"></asp:TextBox>
+        <asp:Button ID="addExpenseButton" runat="server" Text="Add Expense" OnClick="addExpenseButton_Click" />
+
+    </div>
+    <div>
+
+        <asp:Label ID="expenseLabel" runat="server" Text="Expense"></asp:Label>
+        <asp:Label ID="amountLabel" runat="server" Text="Amount"></asp:Label>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ budgetTracker/budgetTracker/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace budgetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Declaring variables
+        static int i = 1;
+        static int j = 1;
+
+        //upon clicking save salary button the number in the text box will be stored in a label
+        protected void saveSalaryButton_Click(object sender, EventArgs e)
+        {
+            monthlySalaryLabel.Text = salaryInput.Text;
+            salaryInput.Text = String.Empty;
+        }
+
+        //This function adds a new label for every expense
+        private void addExpenseNameLabel()
+        {
+            Label lbl = new Label();
+            lbl.ID = "expense" + i.ToString();
+            lbl.Text = expenseNameInput.Text;
+            this.Controls.Add(lbl);
+            expenseNameInput.Text = String.Empty;
+            i++;
+        }
+
+        //This function adds a new amount label for every expense
+        private void addExpenseAmountLabel()
+        {
+            Label lbl = new Label();
+            lbl.ID = "expenseAmount" + j.ToString();
+            lbl.Text = "$" + expenseAmountInput.Text;
+            this.Controls.Add(lbl);
+            j++;
+        }
+
+        //This function updates the remianing monthly budget
+        private void updateRemainingBudget()
+        {
+            decimal monthlySalary;
+            decimal expenseAmount;
+            decimal.TryParse(monthlySalaryLabel.Text, out monthlySalary);
+            decimal.TryParse(expenseAmountInput.Text, out expenseAmount);
+            decimal newMonthlySalary = (monthlySalary - expenseAmount);
+            monthlySalaryLabel.Text = newMonthlySalary.ToString();
+            expenseAmountInput.Text = String.Empty;
+        }
+
+        protected void addExpenseButton_Click(object sender, EventArgs e)
+        {
+            addExpenseNameLabel();
+            addExpenseAmountLabel();
+            updateRemainingBudget();
+        }
+    }
+}
