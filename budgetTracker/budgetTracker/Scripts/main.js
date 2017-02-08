6eb999c154a081c30b0f973ad2a7a3d49d24e095
Kirby Simmons <kirbyjamessimmons@gmail.com>
2017-02-07 23:21:17 -0600

    Added jquery to prepend thead

---
+++ budgetTracker/budgetTracker/Scripts/main.js
@@ -0,0 +1,10 @@
+﻿(function ($) {
+    $(document).ready(function () {
+        $('#tableExpenses').prepend('<thead>' +
+                                     '<tr>' +
+                                       '<th>Expense</th>' +
+                                       '<th>Amount</th>' +
+                                     '</tr>' +
+                                    '</thead>');
+    });
+})(jQuery);
