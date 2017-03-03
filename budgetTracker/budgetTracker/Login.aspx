0dd1e364ffa93bd36fc7cd247ed077f5d09ad3b9
garrettpen <garrettpen@gmail.com>
2017-03-02 20:29:14 -0600

    Added Login page and user database

---
+++ budgetTracker/budgetTracker/Login.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="budgetTracker.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="content/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="content/custom.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <div class="container">
+            <div class="center">
+               <div class="vert">
+
+                <p>
+                    <asp:Label ID="userNameLabel" CssClass="loginBox" runat="server" Text="User Name:"></asp:Label>
+                    <asp:TextBox ID="userNameBox" runat="server"></asp:TextBox>
+                </p>
+                <p>
+                    <asp:Label ID="passwordLabel" CssClass="loginBox" runat="server" Text="Password"></asp:Label>
+                    <asp:TextBox ID="passwordBox" runat="server" ></asp:TextBox>
+                </p>
+
+                <p>
+                    <asp:Button ID="loginButton" runat="server" Text="Login" OnClick="loginButton_Click" /></p>
+                   </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ budgetTracker/budgetTracker/Login.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace budgetTracker
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["UserDataConnectionString"].ConnectionString);
+            conn.Open();
+            string checkUser = "select count(*) from userData where userName='"+userNameBox.Text+"'";
+            SqlCommand com = new SqlCommand(checkUser, conn);
+            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+            conn.Close();
+            if (temp == 1)
+            {
+                conn.Open();
+                string checkPasswordQuery = "select password from userData where userName='" + userNameBox.Text + "'";
+                SqlCommand passComm = new SqlCommand(checkPasswordQuery, conn);
+                string password = passComm.ExecuteScalar().ToString();
+                conn.Close();
+                if (password == passwordBox.Text)
+                {
+                    Response.Redirect("~/Default.aspx");
+                }
+                else
+                {
+                    Response.Write("Incorrect password. Please try again.");
+                }
+            }
+            else
+            {
+                Response.Write("Incorrect user name. Please try again.");
+            }
+        }
+
+    }
+}
